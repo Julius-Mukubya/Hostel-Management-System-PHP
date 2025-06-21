e38b03c23f86c36e95e60e78be82a8ae441e6d91
Julius <mukubyajulius@gmail.com>
2025-06-21 22:42:12 +0300

    finished first version of admin dashboard

--- components/hostel-admin-dashboard/global/side-bar.php
+++ components/hostel-admin-dashboard/global/side-bar.php
@@ -1,6 +1,6 @@
 <!-- Sidebar for Desktop -->
 <div class="sidebar d-none d-md-block">
-    <h3 class="text-white px-3 pt-3">�� Hostel Admin</h3>
+    <h3 class="text-white px-3 pt-3">🏨 Hostel Admin</h3>
     <hr class="text-white" />
     <?php include '_sidebar-links.php'; ?>
 </div>
